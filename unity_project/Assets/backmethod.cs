8af59d5312a5764af80457001b77ec905d40fa5e
Gishandamindu <gishankalasinghe1999@gmail.com>
2024-03-03 21:05:00 +0530

    update

--- unity_project/Assets/backmethod.cs
+++ unity_project/Assets/backmethod.cs
@@ -1,16 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Networking;
 using UnityEngine.SceneManagement;
 
 public class backmethod : MonoBehaviour
 {
-    // Start is called before the first frame update
     public void GoBackToStartScene()
     {
-        // Load the first scene (index 0)
-        SceneManager.LoadScene(0);
-        Debug.Log("Cal");
+        // Check if the start scene is already loaded
+        if (!SceneManager.GetSceneByName("gish").isLoaded)
+        {
+            // Load the start scene only if it's not already loaded
+            SceneManager.LoadScene("gish");
+            Debug.Log("Called");
+        }
     }
 }
